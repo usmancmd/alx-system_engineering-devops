9f92c7d7d9299636adc0b233000123ef3ab6032e
Usman Adepoju <adepojuusman465@gmail.com>
2023-07-24 23:25:23 +0100

    2-export_to_JSON.py

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Returns information for a given employee ID
+and export data in the CSV format.
+"""
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    with open("{}.json".format(user.get("id")), "w") as jsonfile:
+        json.dump({user.get("id"): [{
+                "task": item.get("title"),
+                "completed": item.get("completed"),
+                "username": user.get("id")
+             } for item in todos]}, jsonfile)
