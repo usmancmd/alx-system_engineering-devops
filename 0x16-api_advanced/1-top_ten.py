2868d0aa2ca8be020f4a07ef3207a848fbe2835c
Usman Adepoju <adepojuusman465@gmail.com>
2023-08-08 12:16:29 +0100

    1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+prints the titles of the first 10 hot posts listed for a given subreddit
+"""
+import requests
+
+
+def top_ten(subreddit):
+    headers = {'User-Agent': 'MyRedditBot'}
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        posts = data["data"]["children"]
+
+        for index, post in enumerate(posts[:10], start=1):
+            title = post["data"]["title"]
+            print(f"{index}. {title}")
+    else:
+        return 0
