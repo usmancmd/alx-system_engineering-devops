f736b1e57f65f1313597b48b10dc733116abb018
Usman Adepoju <adepojuusman465@gmail.com>
2023-08-08 12:18:35 +0100

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,11 +8,11 @@ import requests
 
 def number_of_subscribers(subreddit):
     headers = {'User-Agent': 'MyRedditBot'}
-    
+
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    
+
     response = requests.get(url, headers=headers)
-    
+
     if response.status_code == 200:
         data = response.json()
         subscribers = data['data']['subscribers']
