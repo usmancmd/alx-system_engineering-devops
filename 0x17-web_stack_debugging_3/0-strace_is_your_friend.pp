3d2ca02f7fa3ebcb613a29a19cf268ecefde4bd6
Usman Adepoju <adepojuusman465@gmail.com>
2023-08-10 14:14:06 +0100

    0-strace_is_your_friend.pp

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fix Apache server from returning 500 error using strace
+
+exec { 'fix':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
