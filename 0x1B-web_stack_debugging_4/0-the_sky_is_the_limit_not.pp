d3e63fffb6b89fdd0d8cb49236d0e0be3d6b7d4b
Usman Adepoju <adepojuusman465@gmail.com>
2023-08-14 16:32:14 +0100

    0-the_sky_is_the_limit_not.pp

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+#Increase amount of traffic request
+
+exec { 'increase-traffic':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'restart-nginx':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
